928303111aa5494e65ff56be22728b8479576be9
Tomasz Dziurko <tdziurko@gmail.com>
2011-08-28 16:15:38 +0200

    Hide table panel at start when there are no pasties

--- src/pl/softwaremill/idea/pastieplugin/history/HistoryPanel.java
+++ src/pl/softwaremill/idea/pastieplugin/history/HistoryPanel.java
@@ -110,7 +110,7 @@ public class HistoryPanel {
         historyTablePanel = new JPanel();
         historyTablePanel.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
         historyTablePanel.setEnabled(true);
-        historyTablePanel.setVisible(true);
+        historyTablePanel.setVisible(false);
         rootPanel.add(historyTablePanel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         historyTableScrollPane = new JScrollPane();
         historyTableScrollPane.setVisible(true);
